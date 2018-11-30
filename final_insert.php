70d9dbeb793bd94d67859cd1a3b38ebd6eec7a3f
misbahasim <misbahasim789@gmail.com>
2018-11-30 15:24:10 +0500

    including all files

---
+++ final_insert.php
@@ -0,0 +1,24 @@
+<?php
+$con=mysqli_connect('localhost' ,'root' ,'') ;
+
+if(!$con){
+echo"Not connected";
+}
+
+if (!mysqli_select_db($con,'test'))
+{
+echo"database not selcted";
+}
+
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$mail=$_POST['email'];
+$message=$_POST['message'];
+$sql ="INSERT INTO contact(name,phone,email,message) VALUES ('$name','$phone','$mail','$message')";
+if(!mysqli_query($con,$sql)){
+echo"not inserted";
+}
+else {
+echo"Your Form has been submitted successfully :)";
+}
+?>
